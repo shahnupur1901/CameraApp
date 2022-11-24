9d202ae56819eb2c3f58b27f2513fff66a97856e
shahnupur1901 <shahnupur1901@gmail.com>
2022-11-24 16:24:47 +0530

    Dependencies added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import ImageView from './WiproCamera/ImageView';
 function App() {
   return (
     <div className="App">
