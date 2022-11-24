234b292c6d3dfa4f70e4cd9e6c27be70886af7c8
shahnupur1901 <shahnupur1901@gmail.com>
2022-11-24 16:16:41 +0530

    Deleting excess data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Parse from 'parse/dist/parse.min.js';
-import ImageView from "./WiproCamera/ImageView"
-const PARSE_APPLICATION_ID = 'pDDlM9eMAMetxB7yIZQCZB38t0N2NZvsIanQ0xIp';
-const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
-const PARSE_JAVASCRIPT_KEY = 'XlveY2urQm4OQd9qfsIHsxPRuFPNhcAZGBnRWiRW';
-Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
-Parse.serverURL = PARSE_HOST_URL;
+
 function App() {
   return (
     <div className="App">
