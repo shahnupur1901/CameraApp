7fbe1011121eeb5e0b29abe137c33de86ed31403
shahnupur1901 <shahnupur1901@gmail.com>
2022-11-25 15:39:25 +0530

    changes

--- src/camera/index.js
+++ src/camera/index.js
@@ -100,7 +100,6 @@ export function Camera({ onCapture, onClear }) {
 
             <Overlay hidden={!isVideoPlaying} />
 
-
             <Flash
               flash={isFlashing}
               onAnimationEnd={() => setIsFlashing(false)}
