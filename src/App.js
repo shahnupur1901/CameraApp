1d0a78b05feb092e32b585f3aecc66c537c22275
shahnupur1901 <shahnupur1901@gmail.com>
2022-11-26 13:25:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Camera } from "./camera/illndex";
 import { Root, Preview, Footer, GlobalStyle } from "./styles";
 import { useUserMedia } from "./hooks/use-user-media";
 import ImageView from "./WiproCamera/ImageView";
+import resemble from "resemblejs"
 import { margin } from "@mui/system";
 // import {
 //   Video,
@@ -64,6 +65,7 @@ function App(){
   const [videoDem, handleVideoDem] = useState({w:0, h:0})
   const [prevImages, setPrevImages] = useState([])
   const [prevPreview, setPrevPreview] = useState(null)
+  const [mismatch, setMismatch] = useState(0)
   const getVideo = () =>{
     navigator.mediaDevices.getUserMedia({
       audio: false,
@@ -95,33 +97,60 @@ function App(){
       ctx.drawImage(video,width*0.25, 0, width*0.75, height, 0,0, width*0.75, height)
     }
     var image = canvas.toDataURL("image/jpeg").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-    console.log(image)
+   
     setPrevImages([...image])
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    console.log(width)
     canvas.height = height
     canvas.width = 0.25*width
     ctx.drawImage(video,width*0.75, 0, width*0.25, height, 0,0, width*0.25, height)
     var image2 = canvas.toDataURL("image/jpeg").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-    console.log(image2)
     setPrevPreview(image2)
-
   }
   useEffect(()=>{
     getVideo();
   },[videoRef])
+
+  const giveRecommendation = ()=>{
+    if(prevImages.length > 0 ){
+    const width = videoDem.w
+    const height = videoDem.h
+    let video = videoRef.current
+    let photo = photoRef.current
+    photo.width = width
+    photo.height = height
+    let ctx = photo.getContext('2d')
+    let canvas= document.getElementById('canvasImage');
+    canvas.width = 0.25*width
+    ctx.drawImage(video,0, 0, width*0.25, height, 0,0, width*0.25, height)
+    var image2 = canvas.toDataURL("image/jpeg").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+      console.log(image2)
+      console.log(prevPreview)
+   resemble.outputSettings({ useCrossOrigin: false });
+  resemble(image2).compareTo(prevPreview).ignoreLess().onComplete((data)=>{
+    console.log(data)
+    setMismatch(data.misMatchPercentage)
+  })
+   
+    }
+  }
+  setInterval(()=>{
+    giveRecommendation()
+  },100)
+  const getColor = ()=>{
+    return mismatch > 50 ? "red" : "green"
+  }
   return (
     <>
     <div className="App" style={{display:"flex", flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop:"0px", overflow:"hidden"}}>
-      
+
       <div className="camera" style={{position:"relative"}}>
       {prevImages.length>0 && <img src={`${prevPreview}`} style={{opacity : "0.5", position:"absolute", top:"0", left:"0", margin:"0"}}></img>}
-        <video ref = {videoRef}></video>
+        <video ref = {videoRef} ></video>
         <canvas ref={photoRef} id="canvasImage" hidden={true}></canvas>
       </div>
       </div>
       <div className="App" style={{display:"flex",justifyContent: "center", alignItems: "center"}}>
-      <button onClick={takePhoto}>Click</button>
+      <button onClick={takePhoto} style={{color: getColor()}}>Click</button>
       </div>
    
        </>
